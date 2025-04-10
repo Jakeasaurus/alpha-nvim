197873ad51abd08e0b22d2c225b8a73730750ce1
jakeasaurus <jjones@slfcu.org>
2025-04-10 10:44:56 -0400

    updated shortcuts

--- lua/alpha/themes/dashboard.lua
+++ lua/alpha/themes/dashboard.lua
@@ -76,12 +76,10 @@ local buttons = {
     type = "group",
     val = {
         button("e", "  New file", "<cmd>ene <CR>"),
-        button("SPC f f", "󰈞  Find file"),
-        button("SPC f h", "󰊄  Recently opened files"),
-        button("SPC f r", "  Frecency/MRU"),
-        button("SPC f g", "󰈬  Find word"),
-        button("SPC f m", "  Jump to bookmarks"),
-        button("SPC s l", "  Open last session"),
+        button("SPC s f", "󰈞  Find file"),
+        button("SPC s .", "󰊄  Recently opened files"),
+        button("SPC s w", "󰈬  Find word"),
+        button("/", "Open Explorer"),
     },
     opts = {
         spacing = 1,
